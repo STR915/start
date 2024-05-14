904d73b0fe6227d520578d95b7fb7cfa6bfa9f3e
str@lenovo <str@lenovo.com>
2024-05-14 21:46:41 +0800

    1.02

--- source/kernel/cpu/cpu.c
+++ source/kernel/cpu/cpu.c
@@ -39,6 +39,10 @@ void init_gdt(void){
     segment_desc_set(KERNEL_SELECTOR_CS, 0, 0xFFFFFFFF,
     SEG_P_PRESENT | SEG_DPL0 | SEG_S_NORMAL | SEG_TYPE_CODE | SEG_D);
 
+    gate_desc_set((gate_desc_t*)(gdt_table + (SELECTOR_SYSCALL >> 3)),
+        KERNEL_SELECTOR_CS, (uint32_t)exception_handler_syscall,
+        GATE_P_PRESENT | GATE_DPL3 | GATE_TYPE_SYSCALL | SYSCALL_PARAM_COUNT
+    );
     lgdt((uint32_t)gdt_table, sizeof(gdt_table));
 }
 
